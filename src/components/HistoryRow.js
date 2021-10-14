09e9a899ef235edc7a7d95e8063065cf2be4bedb
nayanbari <nayanbari89@gmail.com>
2021-10-14 20:15:04 +0530

    navigation

---
+++ src/components/HistoryRow.js
@@ -0,0 +1,31 @@
+import '../styles/table.css'
+
+const HistoryRow = () => {
+    return (
+        <div className="row">
+                <div className="number">
+                    <h4>1</h4>
+                </div>
+                <div className="name">
+                    <h4>Note</h4>
+                </div>
+                <div className="price">
+                    <h4>180</h4>
+                </div>
+                <div className="amount">
+                    <h4>2</h4>
+                </div>
+                <div className="total">
+                    <h4>360</h4>
+                </div>
+                <div className="actions">
+                    <h4>
+
+                    12-01-2021
+                    </h4>
+                </div>
+        </div>
+    )
+}
+
+export default HistoryRow
